2ef2a4798936aff65a63dbfcd1da6630ffeff465
Chloe120706 <wzq120706@outlook.com>
2025-08-29 14:29:43 +0000

    sign in

---
+++ streamlit.app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title(" My testing app:")
+st.write("Let's start !")
+usernames = ["123456"]
+passwords = ["123456"] 
+username = []
+password = []
+
+def sign(inorup):
+    st.dialog("Sign %s:" % inorup)
+    st.write("Username:")
+    username.append(st.text_input("Write your username."))
+    st.write("Password:")
+    password.append(st.text_input("Write your password."))
+    if st.button("Submit"):
+        if username in usernames:
+            if password == passwords(usernames.index(username)):
+                st.write("Welcome %s !" % username)
+            else:
+                st.write("Your username or password is wrong, please try again!")
+        else:
+            st.write("Your username or password is wrong, please try again!")
+
+if st.button("Sign in"):
+    sign("in")
